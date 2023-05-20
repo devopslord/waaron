ac216f0b1cf88cd4d4e66215f2042b9c3b8ebd93
Sheriff <realexcel2020@gmail.com>
2023-05-20 16:23:52 +0100

    Added the new frontend feature

---
+++ src/frontend/scripts/visitCounter.js
@@ -0,0 +1,18 @@
+/*jshint esversion: 8 */
+
+const p = document.querySelector("#visit-counter");
+VISITORS_ENDPOINT =
+  "https://cprrj9snd2.execute-api.eu-west-1.amazonaws.com/visitors";
+
+const updateCounter = async (event) => {
+  const response = await fetch(VISITORS_ENDPOINT, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+  const visitors = await response.json();
+  p.textContent = `This page has been visited ${visitors.visits} times`;
+};
+
+window.addEventListener("DOMContentLoaded", updateCounter);
